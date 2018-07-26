2a80fe796396eec07247aec4d7e27723048a10c5
purulalwani <purulalwani@gmail.com>
2018-07-26 19:00:12 +0530

    update network and deployed chaincode

--- channelconfig.sh
+++ channelconfig.sh
@@ -1,13 +1,15 @@
 ./bin/configtxgen -profile SupplyChainVehicleGenesis -outputBlock ./channel-artifacts/genesis.block
 
-./bin/configtxgen -profile SupplyChainVehicleChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID supplychannel
+./bin/configtxgen -profile SupplyChainVehicleChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID vt-channel
 
-./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/OwnerMSPanchors.tx -channelID supplychannel -asOrg OwnerMSP
+./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/OwnerMSPanchors.tx -channelID vt-channel -asOrg OwnerMSP
 
-./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/ManufacturerMSPanchors.tx -channelID supplychannel -asOrg ManufacturerMSP
+./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/ManufacturerMSPanchors.tx -channelID vt-channel -asOrg ManufacturerMSP
 
-./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/TransporterMSPanchors.tx -channelID supplychannel -asOrg TransporterMSP
+./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/SupplierMSPanchors.tx -channelID vt-channel -asOrg SupplierMSP
 
-./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/InsurerMSPanchors.tx -channelID supplychannel -asOrg InsurerMSP
+./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/DealerMSPanchors.tx -channelID vt-channel -asOrg DealerMSP
 
-./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/WarehouseMSPanchors.tx -channelID supplychannel -asOrg WarehouseMSP
+./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/RegulatorMSPanchors.tx -channelID vt-channel -asOrg RegulatorMSP
+
+./bin/configtxgen -profile SupplyChainVehicleChannel -outputAnchorPeersUpdate ./channel-artifacts/ScrapperMSPanchors.tx -channelID vt-channel -asOrg ScrapperMSP
--- setupnetwork.sh
+++ setupnetwork.sh
@@ -1,32 +1,66 @@
 # Create the channel
-docker exec -e "CORE_PEER_ID=cli" cli peer channel create -o orderer.vt.com: -c supplychannel -f /etc/hyperledger/configtx/channel.tx
+docker exec -e "CORE_PEER_ID=cli" cli peer channel create -o orderer.vt.com:7050 -c vt-channel -f /etc/hyperledger/configtx/channel.tx
 sleep 3
 # Join peer0.manufacturer.vt.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=ManufacturerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/manufacturer.vt.com/users/Admin@manufacturer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.manufacturer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b supplychannel.block
+docker exec -e "CORE_PEER_LOCALMSPID=ManufacturerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/manufacturer.vt.com/users/Admin@manufacturer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.manufacturer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b vt-channel.block
 
-# Join peer0.manufacturer.vt.com to the channel.
+# Join peer0.owner.vt.com to the channel.
 
-docker exec -e "CORE_PEER_LOCALMSPID=AnchorMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/veefin/hyperledger/fabric/peer/crypto/peerOrganizations/anchor.veefin.com/users/Admin@anchor.veefin.com/msp" -e "CORE_PEER_ADDRESS=peer0.anchor.veefin.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b supplychannel.block
+docker exec -e "CORE_PEER_LOCALMSPID=OwnerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/owner.vt.com/users/Admin@owner.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.owner.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b vt-channel.block
 
-# Join peer0.manufacturer.vt.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/veefin/hyperledger/fabric/peer/crypto/peerOrganizations/seller.veefin.com/users/Admin@seller.veefin.com/msp" -e "CORE_PEER_ADDRESS=peer0.seller.veefin.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b supplychannel.block
+# Join peer0.supplier.vt.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=SupplierMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/supplier.vt.com/users/Admin@supplier.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.supplier.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b vt-channel.block
 
+# Join peer0.dealer.vt.com to the channel.
+
+docker exec -e "CORE_PEER_LOCALMSPID=DealerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/dealer.vt.com/users/Admin@dealer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.dealer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b vt-channel.block
+
+# Join peer0.regulator.vt.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=RegulatorMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/regulator.vt.com/users/Admin@regulator.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.regulator.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b vt-channel.block
+
+# Join peer0.scrapper.vt.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=ScrapperMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/scrapper.vt.com/users/Admin@scrapper.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.scrapper.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer channel join -b vt-channel.block
+
+
+
+sleep 3
 
 # Install chaincode to peer0.manufacturer.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=ManufacturerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/manufacturer.vt.com/users/Admin@manufacturer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.manufacturer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n vt-network -v 1.0 -l golang -p supply/go/
+
+# Install chaincode to peer0.owner.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=OwnerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/owner.vt.com/users/Admin@owner.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.owner.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n vt-network -v 1.0 -l golang -p supply/go/
+
+
+# Install chaincode to peer0.supplier.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=SupplierMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/supplier.vt.com/users/Admin@supplier.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.supplier.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n vt-network -v 1.0 -l golang -p supply/go/
+
+# Install chaincode to peer0.dealer.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=DealerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/dealer.vt.com/users/Admin@dealer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.dealer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n vt-network -v 1.0 -l golang -p supply/go/
+
+# Install chaincode to peer0.regulator.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=RegulatorMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/regulator.vt.com/users/Admin@regulator.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.regulator.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n vt-network -v 1.0 -l golang -p supply/go/
+
+# Install chaincode to peer0.scrapper.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=ScrapperMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/scrapper.vt.com/users/Admin@scrapper.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.scrapper.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n vt-network -v 1.0 -l golang -p supply/go/
+
+
+
 sleep 3
-docker exec -e "CORE_PEER_LOCALMSPID=ManufacturerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/veefin/hyperledger/fabric/peer/crypto/peerOrganizations/manufacturer.vt.com/users/Admin@manufacturer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.manufacturer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n supply-network -v 5.14 -l golang -p supply/go/
+# Instantiate and initialize chaincode to peer0.manufacturer.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=ManufacturerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/manufacturer.vt.com/users/Admin@manufacturer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.manufacturer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.vt.com:7050 -C vt-channel -n vt-network -l golang -v 1.0 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','OwnerMSP.member','SupplierMSP.member','DealerMSP.member','RegulatorMSP.member','ScrapperMSP.member')"
 
-# Install chaincode to peer0.Anchor.veefin.com
+# Instantiate and initialize chaincode to peer0.owner.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=OwnerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/owner.vt.com/users/Admin@owner.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.owner.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.vt.com:7050 -C vt-channel -n vt-network -l golang -v 1.0 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','OwnerMSP.member','SupplierMSP.member','DealerMSP.member','RegulatorMSP.member','ScrapperMSP.member')"
 
-docker exec -e "CORE_PEER_LOCALMSPID=AnchorMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/veefin/hyperledger/fabric/peer/crypto/peerOrganizations/anchor.veefin.com/users/Admin@anchor.veefin.com/msp" -e "CORE_PEER_ADDRESS=peer0.anchor.veefin.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n supply-network -v 5.14 -l golang -p supply/go/
+# Instantiate and initialize chaincode to peer0.supplier.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=SupplierMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/supplier.vt.com/users/Admin@supplier.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.supplier.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.vt.com:7050 -C vt-channel -n vt-network -l golang -v 1.0 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','OwnerMSP.member','SupplierMSP.member','DealerMSP.member','RegulatorMSP.member','ScrapperMSP.member')"
 
-# Install chaincode to peer0.Seller.veefin.com
-docker exec -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/veefin/hyperledger/fabric/peer/crypto/peerOrganizations/seller.veefin.com/users/Admin@seller.veefin.com/msp" -e "CORE_PEER_ADDRESS=peer0.seller.veefin.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode install -n supply-network -v 5.14 -l golang -p supply/go/
+# Instantiate and initialize chaincode to peer0.dealer.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=DealerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/dealer.vt.com/users/Admin@dealer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.dealer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.vt.com:7050 -C vt-channel -n vt-network -l golang -v 1.0 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','OwnerMSP.member','SupplierMSP.member','DealerMSP.member','RegulatorMSP.member','ScrapperMSP.member')"
 
-sleep 3
-# Instantiate and initialize chaincode
-docker exec -e "CORE_PEER_LOCALMSPID=ManufacturerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/veefin/hyperledger/fabric/peer/crypto/peerOrganizations/manufacturer.vt.com/users/Admin@manufacturer.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.manufacturer.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.veefin.com: -C supplychannel -n supply-network -l golang -v 5.14 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','AnchorMSP.member','SellerMSP.member')"
+# Instantiate and initialize chaincode to peer0.regulator.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=RegulatorMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/regulator.vt.com/users/Admin@regulator.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.regulator.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.vt.com:7050 -C vt-channel -n vt-network -l golang -v 1.0 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','OwnerMSP.member','SupplierMSP.member','DealerMSP.member','RegulatorMSP.member','ScrapperMSP.member')"
 
-docker exec -e "CORE_PEER_LOCALMSPID=AnchorMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/veefin/hyperledger/fabric/peer/crypto/peerOrganizations/anchor.veefin.com/users/Admin@anchor.veefin.com/msp" -e "CORE_PEER_ADDRESS=peer0.anchor.veefin.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.veefin.com: -C supplychannel -n supply-network -l golang -v 5.14 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','AnchorMSP.member','SellerMSP.member')"
-
-docker exec -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/veefin/hyperledger/fabric/peer/crypto/peerOrganizations/seller.veefin.com/users/Admin@seller.veefin.com/msp" -e "CORE_PEER_ADDRESS=peer0.seller.veefin.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.veefin.com: -C supplychannel -n supply-network -l golang -v 5.14 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','AnchorMSP.member','SellerMSP.member')"
+# Instantiate and initialize chaincode to peer0.scrapper.vt.com
+docker exec -e "CORE_PEER_LOCALMSPID=ScrapperMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/supply/hyperledger/fabric/peer/crypto/peerOrganizations/scrapper.vt.com/users/Admin@scrapper.vt.com/msp" -e "CORE_PEER_ADDRESS=peer0.scrapper.vt.com:7051" -e "CORE_PEER_ID=cli" cli peer chaincode instantiate -o orderer.vt.com:7050 -C vt-channel -n vt-network -l golang -v 1.0 -c '{"Args":[""]}' -P "OR ('ManufacturerMSP.member','OwnerMSP.member','SupplierMSP.member','DealerMSP.member','RegulatorMSP.member','ScrapperMSP.member')"
